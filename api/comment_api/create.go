35b906fa102f5163f9749132b3e94d77ab987082
shanyue_guan <2692941496@qq.com>
2026-09-23 22:39:51 +0800

    fix：将热度分数计算挪到事务的外面

--- api/comment_api/create.go
+++ api/comment_api/create.go
@@ -67,13 +67,13 @@ func (CommentApi) CommentCreateView(c *gin.Context) {
 		if err := tx.Create(&comment).Error; err != nil {
 			return err
 		}
-		redis_article.AddHotScore(article.ID, redis_article.ScoreComment)
 		return tx.Model(&article).Update("comment_count", gorm.Expr("comment_count + ?", 1)).Error
 	})
 	if err != nil {
 		res.FailWithMsg("评论失败", c)
 		return
 	}
+	redis_article.AddHotScore(article.ID, redis_article.ScoreComment)
 	redis_article.ClearDetail(article.ID)
 	res.OKWithData(comment, c)
 }
